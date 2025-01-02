25fbb682dfb2e6551f050e5c8e388336cea95b9e
Robert Chaney <rwchaneyjr@yahoo.com>
2025-01-02 12:22:52 -0600

    roads reworked and rearview (#20)
    * roads reworked and rearview
    
    scene robertLevel1`
    
    * update
    
    roads resurfaced and nav mesh put underneath
    
    * Adding navmesh scene prefab
    
    * Revert Level1
    
    ---------
    
    Co-authored-by: Gennaro Cuomo <gennarocc@proton.me>

--- Assets/Scripts/NavMesh.cs
+++ Assets/Scripts/NavMesh.cs
@@ -7,14 +7,20 @@ public class NavMesh : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent agent;
-    [SerializeField] Transform[] waypoints;
-    [SerializeField] private int destinationPoint;
+   // [SerializeField] Transform[] waypoints;
+  //  [SerializeField] private int destinationPoint;
+  //  public GameObject stop1;
+  // public GameObject stop2;
     bool _continue=true;
-  public  void Start()
+    private float timer = 12f;
+    public void Start()
     {
+        // stop1.SetActive(true);
+        // stop2.SetActive(false);
         agent = GetComponent<NavMeshAgent>();
         GetComponent<NavMeshAgent>().speed = 15f;
     }
+/*(    }
 public void GoToNextPoint()
     {
         if (waypoints.Length == 0)
@@ -23,17 +29,30 @@ public void GoToNextPoint()
         var NextPoint = Random.Range(0, waypoints.Length);
         destinationPoint = (destinationPoint + NextPoint) % waypoints.Length;
     }
-    public void OnTriggerEnter(Collider other)
+*/
+ /*  public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("other car")) {
-            _continue = false;
-          agent.transform.position = agent.transform.position;
-        }
+        stop2.SetActive(true);
+       // if (other.gameObject.CompareTag("Player")) {
+           _continue = false;
+         
+       //   agent.transform.position = agent.transform.position;
+       // }
     }
+ */
     public void Update()
     {
-        if (_continue) 
-        agent.destination=(player.position);
+        if (_continue)
+        {
+            agent.SetDestination(player.position);
+           // stop1.SetActive(false);
+        }
+        timer -= Time.deltaTime;
+        if (timer <=0 && !_continue)
+        {
+           // stop2.SetActive(false);
+            agent.SetDestination(player.position);
+        }
       
     }
 }
