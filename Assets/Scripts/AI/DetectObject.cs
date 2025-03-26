4322a7cd732eaefc52066f5c86b193c00094339b
An Vo <35384824+VN-Asianboy@users.noreply.github.com>
2025-03-26 16:58:19 +1100

    rename Car script to AiCar.
    Additionally: Experimenting with the function  that Ai car will stop in front of another object. For now, I only put printing message out.

---
+++ Assets/Scripts/AI/DetectObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObject : MonoBehaviour
+{
+    //REMEMBER to put two trigger box, one in front and other behind the ai car.
+    //Make sure other objects have box collider trigger on in order this to work.
+    //Change box collider value depend on how much responsinse you want to car to have. (i.e: make the box small if you want to car to start stopping when too close to other).
+    [SerializeField] AiCar aiCarScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Once it detect another trigger box. Slow AiCar down.
+        Debug.Log("Something has entered the BoxCollider!");
+    }
+}
