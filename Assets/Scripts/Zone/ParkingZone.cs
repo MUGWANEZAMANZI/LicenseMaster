64c65fec7c6afee9a1046d617cdfe63f9343b9d1
MUGWANEZA MANZI Audace <mmaudace@gmail.com>
2025-03-27 20:14:14 +0200

    Decomposed Level Manager
    Am decomposing Level Manager into
    UIManager, ViolationManager, Finance and Detect.
    We will remove previous features once our decomposition is sucesful.
    @VN-Asianboy  Now you task is to decompose to have AuidoManager and Objective Manager. I have created gameobjects to you under Managers, you can also read my scripts see how am decomposing this.

--- Assets/Scripts/Zone/ParkingZone.cs
+++ Assets/Scripts/Zone/ParkingZone.cs
@@ -6,7 +6,7 @@ public class ParkingZone : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(playerHasEnteredZone);
+        //Debug.Log(playerHasEnteredZone);
         if (playerHasEnteredZone && Mathf.Abs(CarController.speed) < .5) 
         {
             LevelManager.instance.CompleteObjective();
